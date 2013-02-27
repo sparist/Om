650c828ac66ea58589d843fc34ec0b3dd7ffe600
Jason Erb <jason.erb@sparist.com>
2013-02-27 08:53:44 -0500

    Now builds ICU on Windows as static library via Cygwin.

--- premake.bat
+++ premake.bat
@@ -5,27 +5,48 @@ for %%x in (%*) do Set /A argC+=1
 if %argC% lss 2 goto Usage
 if %argc% gtr 2 goto Usage
 
-set OldDirectory=%CD%
-set OldPath=%PATH%
+if not exist %1 goto IcuMissing
+if not exist %2 goto BoostMissing
+
+set Directory=%CD%
 call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat"
 
 cd %1
 set Icu=%CD%
-set PATH=%Icu%\bin\;%PATH%
-msbuild "source\allinone\allinone.sln" /p:Configuration=Debug
-msbuild "source\allinone\allinone.sln" /p:Configuration=Release
-call "source\allinone\icucheck.bat" %PROCESSOR_ARCHITECTURE% Release
-set PATH=%OldPath%
-cd "%OldDirectory%"
+for %%y in ("%Icu%") do set IcuShort=%%~sy
+for /f "delims=" %%a in ('cygpath --unix "%IcuShort%"') do set IcuShortUnix=%%a
+mkdir build
+mkdir make
+cd make
+mkdir debug
+cd debug
+bash ../../source/runConfigureICU --enable-debug --disable-release Cygwin/MSVC --enable-static --prefix="%IcuShortUnix%/build"
+make
+make install
+cd ..
+mkdir release
+cd release
+bash ../../source/runConfigureICU Cygwin/MSVC --enable-static --prefix="%IcuShortUnix%/build"
+make
+make install
+cd "%Directory%"
 
 cd %2
 set Boost=%CD%
-call .\bootstrap --with-icu="%Icu%" --prefix="%Boost%\build"
-call .\b2 boost.locale.icu=on boost.locale.std=off -sICU_PATH="%Icu%" --with-chrono --with-locale --with-system --with-thread --with-test link=static install --prefix="%Boost%\build"
-cd "%OldDirectory%"
+call .\bootstrap --with-icu="%Icu%\build" --prefix="%Boost%\build"
+call .\b2 boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off boost.locale.winapi=off boost.locale.posix=off define=U_STATIC_IMPLEMENTATION=1 -sICU_PATH="%Icu%\build" --hash --with-chrono --with-locale --with-system --with-thread --with-test link=static install --prefix="%Boost%\build"
+cd "%Directory%"
 
 exit /b 0
 
 :Usage
 echo Usage: %0 "ICU path" "Boost path"
+exit /b 1
+
+:IcuMissing
+echo The ICU directory was not found at "%1".
+exit /b 1
+
+:BoostMissing
+echo The Boost directory was not found at "%2".
 exit /b 1
