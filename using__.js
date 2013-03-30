a7b28a514dd5a53c53becce046b34889ef9a48b5
Jason Erb <jason.erb@sparist.com>
2013-03-30 15:08:29 -0400

    Reorganized files and improved the build process.

--- using__.js
+++ using__.js
@@ -2,13 +2,19 @@ var using__ =
 [
     [ "Using the Interpreter", "using_interpreter__.html", null ],
     [ "Using the Code", "using_code__.html", [
-      [ "Dependencies", "using_code__.html#using_code__dependencies__", null ],
+      [ "Platforms", "using_code__.html#using_code__platforms__", [
+        [ "Mac OS X", "using_code__.html#using_code__platforms__mac__", null ],
+        [ "Windows", "using_code__.html#using_code__platforms__windows__", null ],
+        [ "Other", "using_code__.html#using_code__platforms__other__", null ]
+      ] ],
+      [ "Dependencies", "using_code__.html#using_code__dependencies__", [
+        [ "Programs", "using_code__.html#using_code__dependencies__programs__", null ],
+        [ "Libraries", "using_code__.html#using_code__dependencies__libraries__", [
+          [ "ICU4C", "using_code__.html#using_code__dependencies__libraries__icu4c", null ],
+          [ "Boost", "using_code__.html#using_code__dependencies__libraries__boost", null ]
+        ] ]
+      ] ],
       [ "Project", "using_code__.html#using_code__project__", [
-        [ "Platforms", "using_code__.html#using_code__project__platforms__", [
-          [ "Xcode", "using_code__.html#using_code__project__platforms__xcode__", null ],
-          [ "Visual Studio", "using_code__.html#using_code__project__platforms__visual_studio__", null ],
-          [ "Other", "using_code__.html#using_code__project__platforms__other__", null ]
-        ] ],
         [ "Targets", "using_code__.html#using_code__project__targets__", [
           [ "Om", "using_code__.html#using_code__project__targets__om__", null ],
           [ "Om.Test", "using_code__.html#using_code__project__targets__om_test__", null ],
