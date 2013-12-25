af67720024b319198b089794f670a52164b36efc
Jason Erb <jason.erb@sparist.com>
2013-12-25 16:36:38 -0500

    Refactored build scripts.

--- dependencies/Boost/build.cmake
+++ dependencies/Boost/build.cmake
@@ -33,41 +33,38 @@ if(NOT ${Status} EQUAL 0)
 endif()
 
 message(STATUS "Configuring Boost...")
+set(BootstrapCommand ./bootstrap.sh)
 if(WIN32)
-	execute_process(
-		COMMAND bootstrap.bat
-		WORKING_DIRECTORY "${Output}/download/${Download}"
-		RESULT_VARIABLE Status
-	)
-else()
-	execute_process(
-		COMMAND ./bootstrap.sh
-		WORKING_DIRECTORY "${Output}/download/${Download}"
-		RESULT_VARIABLE Status
-	)
+	set(BootstrapCommand bootstrap.bat)
 endif()
+execute_process(
+	COMMAND ${BootstrapCommand}
+	WORKING_DIRECTORY "${Output}/download/${Download}"
+	RESULT_VARIABLE Status
+)
 if(NOT ${Status} EQUAL 0)
 	message(FATAL_ERROR "Boost could not be configured: ${Status}")
 endif()
 
 message(STATUS "Building Boost...")
 set(Build "${Output}/build/${Name}")
+set(BuildCommand ./b2)
+set(DefineOption define=U_CHARSET_IS_UTF8=1)
+set(AddressModelOption)
+set(LinkFlagsOption linkflags=-ldl)
 if(WIN32)
+	set(BuildCommand b2)
+	set(DefineOption define=U_STATIC_IMPLEMENTATION=1)
 	if(CMAKE_CL_64)
 		set(AddressModelOption address-model=64)
 	endif()
-	execute_process(
-		COMMAND b2 boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off boost.locale.winapi=off boost.locale.posix=off define=U_STATIC_IMPLEMENTATION=1 -sICU_PATH="${Icu4c}" --build-dir=${Build}/make --prefix=${Build}/install --hash --with-locale --with-system --with-thread --with-test link=static ${AddressModelOption} install
-		WORKING_DIRECTORY "${Output}/download/${Download}"
-		RESULT_VARIABLE Status
-	)
-else()
-	execute_process(
-		COMMAND ./b2 boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off define=U_CHARSET_IS_UTF8=1 -sICU_PATH="${Icu4c}" --build-dir=${Build}/make --prefix=${Build}/install --with-locale --with-system --with-thread --with-test link=static linkflags=-ldl install
-		WORKING_DIRECTORY "${Output}/download/${Download}"
-		RESULT_VARIABLE Status
-	)
+	set(LinkFlagsOption)
 endif()
+execute_process(
+	COMMAND ${BuildCommand} boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off boost.locale.winapi=off boost.locale.posix=off ${DefineOption} -sICU_PATH="${Icu4c}" --build-dir=${Build}/make --prefix=${Build}/install --hash --with-locale --with-system --with-thread --with-test link=static ${AddressModelOption} ${LinkFlagsOption} install
+	WORKING_DIRECTORY "${Output}/download/${Download}"
+	RESULT_VARIABLE Status
+)
 if(NOT ${Status} EQUAL 0)
 	message(FATAL_ERROR "Boost could not be built: ${Status}")
 endif()
--- dependencies/Icu4c/build.cmake
+++ dependencies/Icu4c/build.cmake
@@ -51,61 +51,6 @@ function(CreateConfigurationDirectory Configuration)
 		message(FATAL_ERROR "The directory \"${Build}/make/${Configuration}\" could not be created: ${Status}")
 	endif()
 endfunction()
-if(WIN32)
-	CreateConfigurationDirectory(Debug)
-endif()
-CreateConfigurationDirectory(Release)
-
-if(WIN32)
-	set(System Cygwin/MSVC)
-
-	if(WIN32)
-		execute_process(
-			COMMAND cygpath --unix "${Build}/install"
-			OUTPUT_VARIABLE Prefix
-			RESULT_VARIABLE Status
-		)
-		if(NOT ${Status} EQUAL 0)
-			message(FATAL_ERROR "The output path could not be converted to Unix format: ${Status}")
-		endif()
-	endif()
-
-	message(STATUS "Configuring ICU4C (Debug)...")
-	execute_process(
-		COMMAND bash ../../../../download/icu/source/runConfigureICU --enable-debug --disable-release ${System} --enable-static --prefix=${Prefix}
-		WORKING_DIRECTORY "${Build}/make/Debug"
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "ICU4C (Debug) could not be configured: ${Status}")
-	endif()
-
-	message(STATUS "Configuring ICU4C (Release)...")
-	execute_process(
-		COMMAND bash ../../../../download/icu/source/runConfigureICU ${System} --enable-static --prefix=${Prefix}
-		WORKING_DIRECTORY "${Build}/make/Release"
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "ICU4C (Release) could not be configured: ${Status}")
-	endif()
-else()
-	if(APPLE)
-		set(System MacOSX)
-	else()
-		set(System Linux)
-	endif()
-
-	message(STATUS "Configuring ICU4C (Release)...")
-	execute_process(
-		COMMAND ../../../../download/icu/source/runConfigureICU ${System} --enable-static --disable-shared --prefix=${Build}/install CPPFLAGS=-DU_CHARSET_IS_UTF8=1
-		WORKING_DIRECTORY "${Build}/make/Release"
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "ICU4C (Release) could not be configured: ${Status}")
-	endif()
-endif()
 
 function(Build Configuration)
 	message(STATUS "Building ICU4C (${Configuration})...")
@@ -119,7 +64,53 @@ function(Build Configuration)
 		message(FATAL_ERROR "ICU4C (${Configuration}) could not be built: ${Status}")
 	endif()
 endfunction()
+
+set(Prefix ${Build}/install)
+set(System)
+set(DisableSharedOption --disable-shared)
+set(CppFlagsOption CPPFLAGS=-DU_CHARSET_IS_UTF8=1)
 if(WIN32)
+	set(System Cygwin/MSVC)
+
+	execute_process(
+		COMMAND cygpath --unix "${Prefix}"
+		OUTPUT_VARIABLE Prefix
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "The output path could not be converted to Unix format: ${Status}")
+	endif()
+
+	set(DisableSharedOption)
+	set(CppFlagsOption)
+
+	message(STATUS "Configuring ICU4C (Debug)...")
+	CreateConfigurationDirectory(Debug)
+	execute_process(
+		COMMAND bash ../../../../download/icu/source/runConfigureICU --enable-debug --disable-release ${System} --enable-static --prefix=${Prefix}
+		WORKING_DIRECTORY "${Build}/make/Debug"
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "ICU4C (Debug) could not be configured: ${Status}")
+	endif()
 	Build(Debug)
+else()
+	if(APPLE)
+		set(System MacOSX)
+	else()
+		set(System Linux)
+	endif()
+endif()
+
+message(STATUS "Configuring ICU4C (Release)...")
+CreateConfigurationDirectory(Release)
+execute_process(
+	COMMAND bash ../../../../download/icu/source/runConfigureICU ${System} --enable-static ${DisableSharedOption} --prefix=${Prefix} ${CppFlagsOption}
+	WORKING_DIRECTORY "${Build}/make/Release"
+	RESULT_VARIABLE Status
+)
+if(NOT ${Status} EQUAL 0)
+	message(FATAL_ERROR "ICU4C (Release) could not be configured: ${Status}")
 endif()
 Build(Release)
