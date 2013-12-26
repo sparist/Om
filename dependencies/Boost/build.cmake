bf34f4408f37fef960c1e8e1bd76087eb04c099f
Jason Erb <jason.erb@sparist.com>
2013-12-26 01:26:36 -0500

    Refactored build scripts into functions.

--- dependencies/Boost/build.cmake
+++ dependencies/Boost/build.cmake
@@ -1,70 +1,66 @@
 cmake_minimum_required(VERSION 2.8.7)
 
-# Arguments:
-# -D Name=[Name]
-# -D MajorVersion=[Major Version]
-# -D MinorVersion=[Minor Version]
-# -D Extension=[Download Extension]
-# -D Md5=[Download MD5]
-# -D Icu4c=[ICU4C Install Directory]
+function(BuildBoost Directory Name MajorVersion MinorVersion Extension Md5 Icu4c)
+	set(Download "boost_${MajorVersion}_${MinorVersion}_0")
 
-set(Download "boost_${MajorVersion}_${MinorVersion}_0")
-
-file(
-	DOWNLOAD "http://sourceforge.net/projects/boost/files/boost/${MajorVersion}.${MinorVersion}.0/${Download}.${Extension}/download" "${Md5}/download/${Download}.${Extension}"
-	STATUS DownloadStatus
-	SHOW_PROGRESS
-	EXPECTED_MD5 ${Md5}
-)
-list(GET DownloadStatus 0 Status)
-if(NOT ${Status} EQUAL 0)
-	list(GET DownloadStatus 1 Error)
-	message(FATAL_ERROR "Boost could not be downloaded: ${Error} (${Status})")
-endif()
-get_filename_component(Output "${Md5}" REALPATH)
-
-execute_process(
-	COMMAND "${CMAKE_COMMAND}" -E tar xvzf "${Download}.${Extension}"
-	WORKING_DIRECTORY "${Output}/download"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "Boost could not be unpacked: ${Status}")
-endif()
-
-message(STATUS "Configuring Boost...")
-set(BootstrapCommand ./bootstrap.sh)
-if(WIN32)
-	set(BootstrapCommand bootstrap.bat)
-endif()
-execute_process(
-	COMMAND ${BootstrapCommand}
-	WORKING_DIRECTORY "${Output}/download/${Download}"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "Boost could not be configured: ${Status}")
-endif()
-
-message(STATUS "Building Boost...")
-set(Build "${Output}/build/${Name}")
-set(BuildCommand ./b2)
-set(DefineOption define=U_CHARSET_IS_UTF8=1)
-set(AddressModelOption)
-set(LinkFlagsOption linkflags=-ldl)
-if(WIN32)
-	set(BuildCommand b2)
-	set(DefineOption define=U_STATIC_IMPLEMENTATION=1)
-	if(CMAKE_CL_64)
-		set(AddressModelOption address-model=64)
+	file(
+		DOWNLOAD "http://sourceforge.net/projects/boost/files/boost/${MajorVersion}.${MinorVersion}.0/${Download}.${Extension}/download" "${Directory}/${Md5}/download/${Download}.${Extension}"
+		STATUS DownloadStatus
+		SHOW_PROGRESS
+		EXPECTED_MD5 ${Md5}
+	)
+	list(GET DownloadStatus 0 Status)
+	if(NOT ${Status} EQUAL 0)
+		list(GET DownloadStatus 1 Error)
+		message(FATAL_ERROR "Boost could not be downloaded: ${Error} (${Status})")
 	endif()
-	set(LinkFlagsOption)
-endif()
-execute_process(
-	COMMAND ${BuildCommand} boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off boost.locale.winapi=off boost.locale.posix=off ${DefineOption} -sICU_PATH="${Icu4c}" --build-dir=${Build}/make --prefix=${Build}/install --hash --with-locale --with-system --with-thread --with-test link=static ${AddressModelOption} ${LinkFlagsOption} install
-	WORKING_DIRECTORY "${Output}/download/${Download}"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "Boost could not be built: ${Status}")
-endif()
+	get_filename_component(Output "${Directory}/${Md5}" REALPATH)
+
+	execute_process(
+		COMMAND "${CMAKE_COMMAND}" -E tar xvzf "${Download}.${Extension}"
+		WORKING_DIRECTORY "${Output}/download"
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "Boost could not be unpacked: ${Status}")
+	endif()
+
+	message(STATUS "Configuring Boost...")
+	set(BootstrapCommand ./bootstrap.sh)
+	if(WIN32)
+		set(BootstrapCommand bootstrap.bat)
+	endif()
+	execute_process(
+		COMMAND ${BootstrapCommand}
+		WORKING_DIRECTORY "${Output}/download/${Download}"
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "Boost could not be configured: ${Status}")
+	endif()
+
+	message(STATUS "Building Boost...")
+	set(Build "${Output}/build/${Name}")
+	set(BuildCommand ./b2)
+	set(DefineOption define=U_CHARSET_IS_UTF8=1)
+	set(HashOption)
+	set(AddressModelOption)
+	set(LinkFlagsOption linkflags=-ldl)
+	if(WIN32)
+		set(BuildCommand b2)
+		set(DefineOption define=U_STATIC_IMPLEMENTATION=1)
+		set(HashOption --hash)
+		if(CMAKE_CL_64)
+			set(AddressModelOption address-model=64)
+		endif()
+		set(LinkFlagsOption)
+	endif()
+	execute_process(
+		COMMAND ${BuildCommand} boost.locale.icu=on boost.locale.std=off boost.locale.iconv=off boost.locale.winapi=off boost.locale.posix=off ${DefineOption} -sICU_PATH="${Icu4c}" --build-dir=${Build}/make --prefix=${Build}/install ${HashOption} --with-locale --with-system --with-thread --with-test link=static ${AddressModelOption} ${LinkFlagsOption} install
+		WORKING_DIRECTORY "${Output}/download/${Download}"
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "Boost could not be built: ${Status}")
+	endif()
+endfunction()
--- dependencies/Icu4c/build.cmake
+++ dependencies/Icu4c/build.cmake
@@ -1,116 +1,107 @@
 cmake_minimum_required(VERSION 2.8.7)
 
-# Arguments:
-# -D Name=[Name]
-# -D MajorVersion=[Major Version]
-# -D MinorVersion=[Minor Version]
-# -D Extension=[Download Extension]
-# -D Md5=[Download MD5]
+function(BuildIcu4c Directory Name MajorVersion MinorVersion Extension Md5)
+	set(Download icu)
 
-set(Download icu)
+	file(
+		DOWNLOAD "http://download.icu-project.org/files/icu4c/${MajorVersion}.${MinorVersion}/icu4c-${MajorVersion}_${MinorVersion}-src.${Extension}" "${Directory}/${Md5}/download/${Download}.${Extension}"
+		STATUS DownloadStatus
+		SHOW_PROGRESS
+		EXPECTED_MD5 ${Md5}
+	)
+	list(GET DownloadStatus 0 Status)
+	if(NOT ${Status} EQUAL 0)
+		list(GET DownloadStatus 1 Error)
+		message(FATAL_ERROR "ICU4C could not be downloaded: ${Error} (${Status})")
+	endif()
+	get_filename_component(Output "${Directory}/${Md5}" REALPATH)
 
-file(
-	DOWNLOAD "http://download.icu-project.org/files/icu4c/${MajorVersion}.${MinorVersion}/icu4c-${MajorVersion}_${MinorVersion}-src.${Extension}" "${Md5}/download/${Download}.${Extension}"
-	STATUS DownloadStatus
-	SHOW_PROGRESS
-	EXPECTED_MD5 ${Md5}
-)
-list(GET DownloadStatus 0 Status)
-if(NOT ${Status} EQUAL 0)
-	list(GET DownloadStatus 1 Error)
-	message(FATAL_ERROR "ICU4C could not be downloaded: ${Error} (${Status})")
-endif()
-get_filename_component(Output ${Md5} REALPATH)
-
-execute_process(
-	COMMAND "${CMAKE_COMMAND}" -E tar xvzf "${Download}.${Extension}"
-	WORKING_DIRECTORY "${Output}/download"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "ICU4C could not be unpacked: ${Status}")
-endif()
-
-execute_process(
-	COMMAND "${CMAKE_COMMAND}" -E make_directory "build/${Name}/make"
-	WORKING_DIRECTORY "${Output}"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "The directory \"${Output}/build/${Name}/make\" could not be created: ${Status}")
-endif()
-get_filename_component(Build "${Output}/build/${Name}" REALPATH)
-
-function(CreateConfigurationDirectory Configuration)
 	execute_process(
-		COMMAND "${CMAKE_COMMAND}" -E make_directory "${Configuration}"
-		WORKING_DIRECTORY "${Build}/make"
+		COMMAND "${CMAKE_COMMAND}" -E tar xvzf "${Download}.${Extension}"
+		WORKING_DIRECTORY "${Output}/download"
 		RESULT_VARIABLE Status
 	)
 	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "The directory \"${Build}/make/${Configuration}\" could not be created: ${Status}")
+		message(FATAL_ERROR "ICU4C could not be unpacked: ${Status}")
 	endif()
+
+	execute_process(
+		COMMAND "${CMAKE_COMMAND}" -E make_directory "build/${Name}/make"
+		WORKING_DIRECTORY "${Output}"
+		RESULT_VARIABLE Status
+	)
+	if(NOT ${Status} EQUAL 0)
+		message(FATAL_ERROR "The directory \"${Output}/build/${Name}/make\" could not be created: ${Status}")
+	endif()
+	get_filename_component(Build "${Output}/build/${Name}" REALPATH)
+
+	function(BuildIcu4cConfiguration Build Configuration Prefix)
+		set(EnableDebugOption)
+		set(DisableReleaseOption)
+		string(COMPARE EQUAL "${Configuration}" "Debug" IsDebug)
+		if(IsDebug)
+			set(EnableDebugOption --enable-debug)
+			set(DisableReleaseOption --disable-release)
+		endif()
+
+		execute_process(
+			COMMAND "${CMAKE_COMMAND}" -E make_directory "${Configuration}"
+			WORKING_DIRECTORY "${Build}/make"
+			RESULT_VARIABLE Status
+		)
+		if(NOT ${Status} EQUAL 0)
+			message(FATAL_ERROR "The directory \"${Build}/make/${Configuration}\" could not be created: ${Status}")
+		endif()
+
+		message(STATUS "Configuring ICU4C (${Configuration})...")
+		set(Command bash)
+		set(DisableSharedOption)
+		set(CppFlagsOption)
+		if(WIN32)
+			set(Command bash)
+			set(System Cygwin/MSVC)
+		else()
+			if(APPLE)
+				set(System MacOSX)
+			else()
+				set(System Linux)
+			endif()
+			set(DisableSharedOption --disable-shared)
+			set(CppFlagsOption CPPFLAGS=-DU_CHARSET_IS_UTF8=1)
+		endif()
+		execute_process(
+			COMMAND ${Command} ../../../../download/icu/source/runConfigureICU ${EnableDebugOption} ${DisableReleaseOption} ${System} --enable-static ${DisableSharedOption} --prefix=${Prefix} ${CppFlagsOption}
+			WORKING_DIRECTORY "${Build}/make/${Configuration}"
+			RESULT_VARIABLE Status
+		)
+		if(NOT ${Status} EQUAL 0)
+			message(FATAL_ERROR "ICU4C (${Configuration}) could not be configured: ${Status}")
+		endif()
+
+		message(STATUS "Building ICU4C (${Configuration})...")
+		execute_process(
+			COMMAND make -s
+			COMMAND make -s install
+			WORKING_DIRECTORY "${Build}/make/${Configuration}"
+			RESULT_VARIABLE Status
+		)
+		if(NOT ${Status} EQUAL 0)
+			message(FATAL_ERROR "ICU4C (${Configuration}) could not be built: ${Status}")
+		endif()
+	endfunction()
+
+	set(Prefix ${Build}/install)
+	if(WIN32)
+		execute_process(
+			COMMAND cygpath --unix "${Prefix}"
+			OUTPUT_VARIABLE Prefix
+			RESULT_VARIABLE Status
+		)
+		if(NOT ${Status} EQUAL 0)
+			message(FATAL_ERROR "The output path could not be converted to Unix format: ${Status}")
+		endif()
+
+		BuildIcu4cConfiguration(${Build} Debug ${Prefix})
+	endif()
+	BuildIcu4cConfiguration(${Build} Release ${Prefix})
 endfunction()
-
-function(Build Configuration)
-	message(STATUS "Building ICU4C (${Configuration})...")
-	execute_process(
-		COMMAND make -s
-		COMMAND make -s install
-		WORKING_DIRECTORY "${Build}/make/${Configuration}"
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "ICU4C (${Configuration}) could not be built: ${Status}")
-	endif()
-endfunction()
-
-set(Prefix ${Build}/install)
-set(System)
-set(DisableSharedOption --disable-shared)
-set(CppFlagsOption CPPFLAGS=-DU_CHARSET_IS_UTF8=1)
-if(WIN32)
-	set(System Cygwin/MSVC)
-
-	execute_process(
-		COMMAND cygpath --unix "${Prefix}"
-		OUTPUT_VARIABLE Prefix
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "The output path could not be converted to Unix format: ${Status}")
-	endif()
-
-	set(DisableSharedOption)
-	set(CppFlagsOption)
-
-	message(STATUS "Configuring ICU4C (Debug)...")
-	CreateConfigurationDirectory(Debug)
-	execute_process(
-		COMMAND bash ../../../../download/icu/source/runConfigureICU --enable-debug --disable-release ${System} --enable-static --prefix=${Prefix}
-		WORKING_DIRECTORY "${Build}/make/Debug"
-		RESULT_VARIABLE Status
-	)
-	if(NOT ${Status} EQUAL 0)
-		message(FATAL_ERROR "ICU4C (Debug) could not be configured: ${Status}")
-	endif()
-	Build(Debug)
-else()
-	if(APPLE)
-		set(System MacOSX)
-	else()
-		set(System Linux)
-	endif()
-endif()
-
-message(STATUS "Configuring ICU4C (Release)...")
-CreateConfigurationDirectory(Release)
-execute_process(
-	COMMAND bash ../../../../download/icu/source/runConfigureICU ${System} --enable-static ${DisableSharedOption} --prefix=${Prefix} ${CppFlagsOption}
-	WORKING_DIRECTORY "${Build}/make/Release"
-	RESULT_VARIABLE Status
-)
-if(NOT ${Status} EQUAL 0)
-	message(FATAL_ERROR "ICU4C (Release) could not be configured: ${Status}")
-endif()
-Build(Release)
